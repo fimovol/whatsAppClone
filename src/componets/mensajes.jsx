9ae4fafa704d63fb6712d7701197e20b990cf1cf
fimovol <fimovolpe@gmail.com>
2022-01-25 12:21:55 -0500

    agregando mas componetes como tipear y otros

---
+++ src/componets/mensajes.jsx
@@ -0,0 +1,7 @@
+export default function _(){
+  return <div className='h-12'>
+    <h1>jejejej</h1>
+    <h1>jejejej</h1>
+    <h1>jejejej</h1>
+  </div>
+}
---
+++ src/componets/tipearmensaje.jsx
@@ -0,0 +1,9 @@
+export default function _(){
+  return  <div className='bg-pink-400 h-16 w-full absolute bottom-0'>
+    <div className='relative w-full h-full flex justify-center items-center'>
+      <div className='absolute w-[98%] h-[90%] bg-slate-700 flex justify-center items-center rounded'>
+        <input type="text" className="h-[90%] w-[99.5%]"/>
+      </div>
+    </div>
+  </div>
+}
